b54685020ef5db7a8f2f179a1759050c8b8b99a3
xiangtao <xiangtao1994@gmail.com>
2025-12-12 00:02:47 +0800

    fix： 补充文件上传

--- app/web/src/api/oss.js
+++ app/web/src/api/oss.js
@@ -13,25 +13,6 @@ async function uploadFile(file) {
     return res.url
 }
 
-/**
- * 上传图片到OSS
- * @param {File} file - 要上传的图片文件
- * @returns {Promise<string>} - 返回拼接后的图片公网URL
- */
-async function uploadImage(file) {
-    return uploadFile(file)
-}
-
-/**
- * 上传视频到OSS
- * @param {File} file - 要上传的视频文件
- * @returns {Promise<string>} - 返回拼接后的视频公网URL
- */
-async function uploadVideo(file) {
-    return uploadFile(file)
-}
-
 export const ossApi = {
-    uploadImage,
-    uploadVideo
+    uploadFile
 }
