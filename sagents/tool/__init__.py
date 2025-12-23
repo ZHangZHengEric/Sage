98349a23d2c4ccf6308f16376b1e70d6143f7883
xiangtao <xiangtao1994@gmail.com>
2025-12-23 18:48:32 +0800

    fix

--- sagents/tool/__init__.py
+++ sagents/tool/__init__.py
@@ -2,7 +2,8 @@ from .execute_command_tool import ExecuteCommandTool
 from .file_parser_tool import FileParserTool
 from .file_system_tool import FileSystemTool
 from .task_interruption_tool import TaskInterruptionTool
-from .tool_base import McpToolSpec, SseServerParameters, ToolBase, ToolSpec
+from .tool_base import ToolBase, ToolSpec
+from .tool_config import McpToolSpec, SseServerParameters
 from .tool_manager import ToolManager
 
 __all__ = [
