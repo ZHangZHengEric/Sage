a029e71da551ebc7d05078f2fc1de5d79acb668b
xiangtao <xiangtao1994@gmail.com>
2025-12-31 01:37:34 +0800

    job 移到指定业务目录

--- app/server/service/kdb.py
+++ app/server/service/kdb.py
@@ -16,7 +16,9 @@ from fastapi import UploadFile
 from utils.id import gen_id
 
 from sagents.utils.logger import logger
+from core.document_parse import get_document_parser
 
+DEFAULT_BATCH_SIZE = 5
 
 class KdbService:
     def __init__(self):
@@ -253,3 +255,67 @@ class KdbService:
         )
         await self.file_dao.insert(f)
         return f
+
+    async def build_waiting_doc(self):
+        docs: List[models.KdbDoc] = (
+            await self.kdb_doc_dao.get_list_by_status_and_data_source(
+                models.KdbDocStatus.PENDING, "file", DEFAULT_BATCH_SIZE
+            )
+        )
+        if not docs:
+            return
+        logger.info(f"开始 处理等待文档, 文档数量: {len(docs)}")
+        for d in docs:
+            await self._process_document(d)
+        logger.info(f"完成 处理等待文档, 文档数量: {len(docs)}")
+
+    async def build_failed_doc(self):
+        docs: List[models.KdbDoc] = await self.kdb_doc_dao.get_failed_list(
+            "file", DEFAULT_BATCH_SIZE
+        )
+        if not docs:
+            return
+        logger.info(f"开始 重新处理失败文档, 文档数量: {len(docs)}")
+        for d in docs:
+            await self._process_document(d)
+        logger.info(f"完成 重新处理失败文档, 文档数量: {len(docs)}")
+
+    async def _process_document(self, doc: models.KdbDoc):
+        await self.kdb_doc_dao.update_status(doc.id, models.KdbDocStatus.PROCESSING)
+        try:
+            # 1. 判断知识库是否存在
+            kdb = await self.kdb_dao.get_by_id(doc.kdb_id)
+            if not kdb:
+                await self.kdb_doc_dao.update_status_and_retry(
+                    doc.id, models.KdbDocStatus.FAILED
+                )
+                logger.error(f"知识库不存在 - KdbId: {doc.kdb_id}")
+                return
+            # 2. 判断文件是否存在
+            file = await self.file_dao.get_by_id(doc.source_id)
+            if not file:
+                await self.kdb_doc_dao.update_status_and_retry(
+                    doc.id, models.KdbDocStatus.FAILED
+                )
+                logger.error(f"文件不存在 - FileId: {doc.source_id}")
+                return
+            parser = get_document_parser(doc.data_source)
+            if parser is None:
+                await self.kdb_doc_dao.update_status_and_retry(
+                    doc.id, models.KdbDocStatus.FAILED
+                )
+                logger.error(f"不支持的文档类型: {doc.data_source}")
+                return
+            await parser.clear_old(kdb.get_index_name(), doc)
+            # 3. 调用文档解析器处理文档
+            await parser.process(kdb.get_index_name(), doc, file)
+            # 调用知识库入库接口，
+            await self.kdb_doc_dao.update_status(doc.id, models.KdbDocStatus.SUCCESS)
+            logger.info(f"处理文档成功 - DocId: {doc.id}, docName: {doc.doc_name}")
+        except Exception as e:
+            await self.kdb_doc_dao.update_status_and_retry(
+                doc.id, models.KdbDocStatus.FAILED
+            )
+            logger.error(
+                f"处理文档失败 - DocId: {doc.id}, docName: {doc.doc_name}, err={e}"
+            )
