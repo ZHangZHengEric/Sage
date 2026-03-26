fe3a5431e3bc2f9080a92bcd27489dfdc693a485
Orzii <497399774@qq.com>
2026-03-27 01:43:46 +0800

    feat: add WeChat Personal (iLink) provider support

---
+++ mcp_servers/im_server/providers/wechat_ilink/__init__.py
@@ -0,0 +1,5 @@
+"""WeChat Personal (iLink) provider for Sage."""
+
+from .provider import WeChatPersonalProvider, WeChatPersonalPoller
+
+__all__ = ["WeChatPersonalProvider", "WeChatPersonalPoller"]
